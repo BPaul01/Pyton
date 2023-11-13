0d7fa2cf0c11b711206269fa7d0e853f0e0f12e1
Bodale Paul <fii.bodalepaul@gmail.com>
2023-11-13 13:46:38 +0200

    Lab5 ex2 done

--- Lab5/main.py
+++ Lab5/main.py
@@ -1,4 +1,5 @@
 import math
+
 #ex1
 class Shape:
     #those functions will be overwritten
@@ -70,24 +71,145 @@ class Triangle(Shape):
     def calculate_perimeter(self):
         return self.a + self.b + self.c
 
+#ex2
+class Account:
+    def __init__(self) -> None:
+        self.amount = 0
+
+class SavingsAccount(Account):
+    def __init__(self) -> None:
+        super().__init__()
+        self.interest_rate = 4 #default interest rate
+        self.current_package = "Default"
+        self.earn = "Yearly"
+        self.amount = 1
+
+    def update_package(self, package):
+        if not isinstance(package, str):
+            return "Error: Please input a valid package"
+
+        if package == "Premium":
+            self.earn = "Monthly"
+            self.interest_rate = 2
+            self.current_package = package
+
+        elif package == "Deluxe":
+            self.earn = "Weekly"
+            self.interest_rate = 0.7
+            self.current_package = package
+
+        elif package == "Default":
+            self.earn = "Yearly"
+            self.interest_rate = 4
+            self.current_package = package
+
+        else:
+            return "Error: Please input a valid package"
+
+    def update_interest_rate(self, new_interest_rate):
+        if not isinstance(new_interest_rate, (int, float)) or new_interest_rate <= 1:
+            return "Error: Please input a valid interest rate"
+
+        if new_interest_rate > 5:
+            print("What are you doing!? Trying to make our costumer rich??")
+            return None
+
+        self.interest_rate = new_interest_rate
+
+    def calculate_interest(self, years):
+        if not isinstance(years, int):
+            return "Error: please enter a valid number of years"
+
+        result = self.amount
+        while years != 0:
+            result = result * self.interest_rate
+            years -= 1
+        
+        return result
+
+    def credit_account(self):
+        self.amount = self.amount * self.interest_rate
+
+class CheckingAccount(Account):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def make_a_deposit(self, amount):
+        if not isinstance(amount, int) or amount <= 0:
+            return "Error: Please input a valid amount"
+        self.amount = self.amount + amount
+
+    def withdraw(self, amount):
+        if not isinstance(amount, int) or amount <= 0:
+            return "Error: Please input a valid amount"
+
+        if self.amount < amount:
+            return "Error: Insufficient founds"
+
+        self.amount = self.amount - amount
+        return self.amount
+
+    def transfer(self, amount, account):
+        if not isinstance(amount, int) or amount <= 0:
+            return "Error: Please input a valid amount"
+
+        if not isinstance(account, Account):
+            return "Error: Invalid account"
+
+        if self.amount < amount:
+            return "Error: Insufficient founds"
+
+        self.amount = self.amount - amount
+        account.amount = account.amount + amount
+        return "Success"
+
 def main():
-    #ex1
-    c = Circle(2)
-    Circle.print_class_name()
-    print("Disk area:", c.calculate_disk_area())
-    print("Circle perimeter:", c.calculate_perimeter())
-    print()
+    # #ex1
+    # c = Circle(2)
+    # Circle.print_class_name()
+    # print("Disk area:", c.calculate_disk_area())
+    # print("Circle perimeter:", c.calculate_perimeter())
+    # print()
 
-    r = Rectangle(2, 3)
-    Rectangle.print_class_name()
-    print("Rectangle area:", r.calculate_area())
-    print("Rectangle perimeter:", r.calculate_perimeter())
-    print()
+    # r = Rectangle(2, 3)
+    # Rectangle.print_class_name()
+    # print("Rectangle area:", r.calculate_area())
+    # print("Rectangle perimeter:", r.calculate_perimeter())
+    # print()
 
-    t = Triangle(4, 5, 6)
-    Triangle.print_class_name()
-    print("Triangle area:", t.calculate_area())
-    print("Triangle perimeter:", t.calculate_perimeter())
+    # t = Triangle(4, 5, 6)
+    # Triangle.print_class_name()
+    # print("Triangle area:", t.calculate_area())
+    # print("Triangle perimeter:", t.calculate_perimeter())
+
+    #ex2
+    s = SavingsAccount()
+    s.credit_account()
+    s.credit_account()
+    print("Account balance:", s.amount)
+    s.update_package("Premium")
+    s.credit_account()
+    print("Account balance:", s.amount)
+    s.update_interest_rate(100)
+    print("Your balance will be:", s.calculate_interest(50), "after 50 years")
+
+    c = CheckingAccount()
+    c.make_a_deposit(200)
+    print("Checking Account balance:", c.amount)
+    result = c.transfer(68, s)
+    if result == "Success":
+        print("Transfer succeded")
+        print("Balance of the account credited:", s.amount)
+        print("Balance of the sender account:", c.amount)
+    else:
+        print(result)
+    result = c.transfer(200, s)
+    if result == "Success":
+        print("Transfer succeded")
+        print("Balance of the account credited:", s.amount)
+        print("Balance of the sender account:", c.amount)
+    else:
+        print(result)
 
 if __name__ == "__main__":
     main()
