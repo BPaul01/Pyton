b4fdfe3e66170b0ca8b0b03ff550ab92dd03e2c0
Bodale Paul <fii.bodalepaul@gmail.com>
2023-11-27 20:36:38 +0200

    Ex3 done

---
+++ Lab6/Ex3/script.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+if len(sys.argv) != 2:
+    raise Exception("Invalid number of parameters")
+
+directory = sys.argv[1]
+
+size = 0
+
+try:
+    if not os.path.exists(directory):
+        raise Exception("Folder doesn't exist")
+
+    if not os.path.isdir(directory):
+        raise Exception("Invalid directory")
+
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            size += os.path.getsize(os.path.join(root, file))
+
+except Exception as e:
+    print(e, file=sys.stderr)
+else:
+    print(f"Total size is: {size}")
