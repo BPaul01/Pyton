7a9c7e1765d705a2979d82da27925eec477bab4f
Bodale Paul <fii.bodalepaul@gmail.com>
2023-11-27 17:42:00 +0200

    Ex1 done

---
+++ Lab6/Ex1/ex1script.py
@@ -0,0 +1,34 @@
+import sys
+import os
+
+if len(sys.argv) < 3:
+    raise Exception("Not enough parameters")
+
+directory = sys.argv[1]
+file_ext = sys.argv[2]
+
+try:
+    if not os.path.isdir(directory):
+        raise Exception("Invalid directory")
+
+    #print(os.listdir(directory))
+    match = [file for file in os.listdir(directory) if file.endswith(file_ext)]
+    if len(match) == 0:
+        raise Exception(f"No files with the extension {file_ext} were found")
+
+    #print(match)
+    try:
+        for file in match:
+            print(f"Content of {os.path.join(directory, file)}:")
+            f = open(os.path.join(directory, file), mode='r')
+            for line in f:
+                print(line.strip())
+            f.close()
+            print()
+    except IOError as e:
+        print(f"Access to file {file} denied", file=sys.stderr)
+
+except Exception as e:
+    print(e, file=sys.stderr)
+
+
---
+++ Lab6/Ex1/search/anotherPyScript.py
@@ -0,0 +1,1 @@
+print("Cuz why not")
