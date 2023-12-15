0e0526a70bed39b19c226fbe431df0bcd49c7cee
Bodale Paul <fii.bodalepaul@gmail.com>
2023-12-15 15:09:54 +0200

    Added Exceptions for Target folder and Source file
    Checked if the file and the folder given correspond to expected proprieties.
    More exceptions might be added later on.

---
+++ Project/create_structure.py
@@ -0,0 +1,43 @@
+import sys
+import os
+
+try:
+    if len(sys.argv) != 3:
+        raise Exception("Error: Invalid number of parameters")
+
+    target_directory = sys.argv[1]
+
+    # check folder existence
+    if not os.path.exists(target_directory):
+        raise Exception("Error: Target folder doesn't exist")
+
+    # check if it is a folder
+    if not os.path.isdir(target_directory):
+        raise Exception("Error: The given path doesn't correspond to a folder")
+
+    # check if it is writeable
+    if not os.access(target_directory, os.W_OK):
+        raise Exception("Error: Target folder is not writeable")
+
+    source_file = sys.argv[2]
+
+    # check file existence
+    if not os.path.exists(source_file):
+        raise Exception("Error: Source file doesn't exist")
+
+    # check if it is a folder
+    if not os.path.isfile(source_file):
+        raise Exception("Error: The given path doesn't correspond to a file")
+
+    # check if it has the right extension
+    if not source_file.endswith(".json"):
+        raise Exception("Error: Expected a .json file")
+
+    # check if it is readable
+    if not os.access(target_directory, os.R_OK):
+        raise Exception("Error: Source file is not readable")
+
+except Exception as e:
+    print(e, file=sys.stderr)
+else:
+    print("All gut")
