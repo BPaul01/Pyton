2dda1bfafae1799bb8574aa0661502c7db4b67f9
Bodale Paul <fii.bodalepaul@gmail.com>
2023-12-18 17:19:27 +0200

    Script now supports working with large .json files
    - implemented solution using ijson library which allows parsing the .json file

--- Project/create_structure.py
+++ Project/create_structure.py
@@ -1,26 +1,95 @@
+import shutil
 import sys
 import os
-import json
+import ijson
 
 
-def create_structure(structure, target_directory):
-    for element in structure:
-        # create all the files at the current level
-        if not isinstance(structure.get(element), dict):
-            try:
-                file = open(os.path.join(target_directory, element), mode="w")
-                file.write(structure.get(element))
-                print(f"File '{element}' created successfully")
-                file.close()
+def reset_folder(folder):
+    try:
+        for item in os.listdir(folder):
+            item_path = os.path.join(folder, item)
+            shutil.rmtree(item_path)
+    except Exception as e:
+        print("Something has gone terribly wrong", file=sys.stderr)
+        raise SystemExit
 
-            except Exception:
-                print(f"Error: Could not create the file {element}", file=sys.stderr)
-        else:
-            # create the new folder
-            os.mkdir(os.path.join(target_directory, element))
+def create_structure(source_file, target_directory):
+    not_the_first = False
+    current_dir = target_directory
 
-            # recursively call the function to create the substructure of the folder
-            create_structure(structure.get(element), os.path.join(target_directory, element))
+    # read the source file and load its contents into a dictionary
+    try:
+        json_data = open(source_file, mode="rb")
+
+        print("Before")
+
+        for prefix, event, value in ijson.parse(json_data):
+
+            print(f'Prefix: {prefix}, Event: {event}, Value: {value}')
+
+            # handle directory creation
+            if event == "start_map" and not_the_first:
+                # get the name of the directory
+                dirs = prefix.split(".")
+                dir_name = dirs[len(dirs) - 1]
+
+                print(dir_name)
+                print(os.listdir(current_dir))
+
+                # check if there are other folders with the same name
+                if dir_name in os.listdir(current_dir):
+                    # revert the target folder
+                    reset_folder(target_directory)
+
+                    raise Exception("Error: Found two folders with the same name at the same level\n"
+                                    "Target folder has been reseted")
+                else:
+                    # create the folder
+                    print(f"Creating the folder {dir_name}")
+                    try:
+                        os.mkdir(os.path.join(current_dir, dir_name))
+                    except Exception as e:
+                        raise Exception("Error: Could not create the folder")
+
+                    # update the current directory
+                    current_dir = os.path.join(current_dir, dir_name)
+
+            else:
+                not_the_first = True
+
+            if event == "map_key":
+                file_name = value
+
+            #handle file creation
+            if event == "string":
+                # # get the name of the file
+                # elements = prefix.split(".")
+                # file_name = elements[len(elements) - 1]
+
+                # check if there are other files with the same name
+                if file_name in os.listdir(current_dir):
+                    # revert the target folder
+                    reset_folder(target_directory)
+
+                    raise Exception("Error: Found two folders with the same name at the same level\n"
+                                    "Target folder has been reseted")
+                else:
+                    # create the file
+                    file = open(os.path.join(current_dir, file_name), mode="w")
+
+                    # write the content of the file
+                    file.write(value)
+                    file.close()
+
+            if event == "end_map":
+                # update the current directory
+                current_dir = os.path.dirname(current_dir)
+
+
+        json_data.close()
+
+    except Exception as e:
+        raise e
 
 
 try:
@@ -59,15 +128,7 @@ try:
     if not os.access(source_file, os.R_OK):
         raise Exception("Error: Source file is not readable")
 
-    # read the source file and load its contents into a dictionary
-    try:
-        s_file = open(source_file, mode="r")
-        structure = json.load(s_file)
-        s_file.close()
-    except Exception as e:
-        raise Exception("Error: Could not open the file")
-
-    create_structure(structure, target_directory)
+    create_structure(source_file, target_directory)
 
 except Exception as e:
     print(e, file=sys.stderr)
